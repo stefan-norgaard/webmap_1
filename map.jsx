86126986f9013922202da3d6d5f28bbb340eb1fd
Stefan-Norgaard <spn2121@columbia.edu>
2020-03-09 13:18:32 -0400

    initial import

--- map.jsx
+++ map.jsx
@@ -86,3 +86,28 @@ map.addSource('nypl',{
 //  });
 
 });
+
+// when the user does a 'click' on an element in the 'trees' layer...
+map.on('click', 'trees', function(e) {
+  // get the map coordinates of the feature
+  var coordinates = e.features[0].geometry.coordinates.slice();
+  // get its species name from the feature's attributes
+  var species = e.features[0].properties.spc_common;
+
+  // and create a popup on the map
+  new mapboxgl.Popup()
+  .setLngLat(coordinates)
+  .setHTML(species)
+  .addTo(map);
+});
+  
+// make the cursor a pointer when over the tree
+map.on('mouseenter', 'trees', function() {
+  map.getCanvas().style.cursor = 'pointer';
+});
+  
+// back to normal when it's not
+map.on('mouseleave', 'trees', function() {
+  map.getCanvas().style.cursor = '';
+});
+
